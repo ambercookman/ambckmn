3ae1d47b560a4d5eb8bf576311e4826f7b67c66a
ambercookman <amber.cookman@gmail.com>
2015-12-07 19:13:24 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Custom WordPress Theme - Portfolio/Blog
+
+Developing a custom theme for my site from _s starter theme.
---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ambckmn
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ambckmn' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'ambckmn' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'ambckmn' ), 'ambckmn', '<a href="http://www.ambercookman.com" rel="designer">Amber Cookman</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
